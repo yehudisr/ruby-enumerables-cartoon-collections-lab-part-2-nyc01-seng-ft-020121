4b26c643f36401cfa378acaeaddfc21cf0077113
yehudis raitman <yehudisr@gmail.com>
2021-01-20 17:43:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,6 +27,6 @@ end
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   planeteer_calls.find do |item|
-    item == valid_calls.find
+    valid_calls.include?(item)
 end
 end
